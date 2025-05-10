2efafc8539165f112cf2c8e4c60315007c2c0baf
Flavio <flaviolunaferreira@gmail.com>
2025-05-10 08:41:29 -0300

    escrevendo estruturas dos consumidores

---
+++ auditoria/auditoria/src/test/java/the/coyote/auditoria/AuditoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package the.coyote.auditoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ notificacoes/notificacoes/src/test/java/the/coyote/notificacoes/NotificacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package the.coyote.notificacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
